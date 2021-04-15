e703778c43459e4fe1ec5195e1a074f5d48f48c0
fanpengfei1 <fanpengfei1@jd.com>
2021-04-15 18:13:37 +0800

    update fo rarima

--- examples/jd_bot/arima/test_arima.py
+++ examples/jd_bot/arima/test_arima.py
@@ -1,16 +1,20 @@
 #美国消费者信心指数
+import itertools
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-import seaborn as sns
-import matplotlib.pylab as plt
-from scipy import  stats
+import seaborn as sns #used for data show advance then matplotlib
+import matplotlib.pylab as plt  #used for data show
+
+from scipy import stats
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.api import qqplot
 
 import time
 import sys
 
 #1.数据预处理
-Sentiment = pd.read_csv('arima/confidence.csv', index_col='date', parse_dates=['date'])
+Sentiment = pd.read_csv('./examples/jd_bot/arima/confidence.csv', index_col='date', parse_dates=['date'])
 #index_col=0, parse_dates=[0]
 print("head:", Sentiment.head())
 print("tail:", Sentiment.tail())
@@ -23,9 +27,9 @@ n_forecast = n_sample - n_train
 ts_train = Sentiment.iloc[:n_train]['confidence']
 ts_test = Sentiment.iloc[:n_forecast]['confidence']
 
+'''
 sentiment_short = Sentiment.loc['2007':'2017']
 sentiment_short.plot(figsize = (12,8))
-'''
 plt.title("Consumer Sentiment")
 plt.legend(bbox_to_anchor = (1.25,0.5))
 sns.despine()
@@ -69,6 +73,7 @@ fig.tight_layout()
 plt.show()
 '''
 
+'''
 #4.2 可视化结果：四个图的整合函数，可以改参数直接调用
 #3.2.可视化结果
 def tsplot(y, lags=None, title='', figsize=(14, 8)):
@@ -92,4 +97,68 @@ def tsplot(y, lags=None, title='', figsize=(14, 8)):
     return ts_ax, acf_ax, pacf_ax
 
 tsplot(sentiment_short, title='Consumer Sentiment', lags=36)
+'''
+
+#4.建立模型——参数选择
+arima200 = ARIMA(ts_train, order=(2,0,0)).fit()#(p,d,q)
+#model_results = arima200.fit()
+
+#遍历，寻找适宜的参数
+p_min = 0
+d_min = 0
+q_min = 0
+d_max = 0
+
+#long time cost
+#p_max = 8
+#q_max = 8
+
+#less time cost
+p_max = 2 #for test
+q_max = 2 #for test
+
+# Initialize a DataFrame to store the results,，以BIC准则
+results_bic = pd.DataFrame(index=['AR{}'.format(i) for i in range(p_min,p_max+1)],
+                           columns=['MA{}'.format(i) for i in range(q_min,q_max+1)])
+
+for p,d,q in itertools.product(range(p_min,p_max+1),range(d_min,d_max+1),range(q_min,q_max+1)):
+    
+    if p==0 and d==0 and q==0:
+        results_bic.loc['AR{}'.format(p), 'MA{}'.format(q)] = np.nan
+        continue
+    try:
+        model = ARIMA(ts_train, order=(p, d, q),)
+        results = model.fit() #	Fit (estimate) the parameters of the model.
+        print("p:%.2f d:%.2f q:%.2f results.bic:%.2f\n" %(p, d, q, results.bic))
+        results_bic.loc['AR{}'.format(p), 'MA{}'.format(q)] = results.bic
+        sys.exit()
+    except:
+        continue
+
+results_bic = results_bic[results_bic.columns].astype(float)
+
+print("results_bic:", results_bic)
+
+#画出BIC热度图结果如下：
+fig, ax = plt.subplots(figsize=(10, 8))
+ax = sns.heatmap(results_bic,
+                 mask=results_bic.isnull(),
+                 ax=ax,
+                 annot=True,
+                 fmt='.2f',
+                 )
+ax.set_title('BIC')
+
+#模型评价准则 check train_results.aic_min_order == train_results.aic_min_order
+train_results = sm.tsa.arma_order_select_ic(ts_train, ic=["aic", "bic"], trend="c", max_ar=4, max_ma=4)
+aic_min_order = train_results.aic_min_order
+bic_min_order = train_results.bic_min_order
+print("AIC:%s BIC:%s" % (aic_min_order, bic_min_order))
+if aic_min_order != bic_min_order:
+    print("Error: BIC check failed!")
+    sys.exit()
+else:
+    print("BIC check pass!")
+
+#### 
 plt.show()
